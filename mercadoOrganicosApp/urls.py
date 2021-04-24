368ab4ca3d7b68d987bca8fe086de4b7ad25809b
JuanR <jd.ruedaa@uniandes.edu.co>
2021-04-24 10:19:04 -0500

    Endpoints

--- mercadoOrganicosApp/urls.py
+++ mercadoOrganicosApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import signin, signout, RegisterView, login_view, redirect_to_home
+from .views import signin, signout, RegisterView, login_view, redirect_to_home, catalogos_list_post, catalogos_update_delete
 from .viewsets import *
 from django.conf.urls import url
 
@@ -26,5 +26,6 @@ urlpatterns = [
     path('signout', signout, name='Sign Out'),
     path('login/', login_view, name='login'),
     path('register/', RegisterView.as_view(), name='auth_register'),
-    path('catalogo/', CatalogoViewset),
+    path('catalogo/', catalogos_list_post, name='Catalogos'),
+    path('catalogo/<int:pk>', catalogos_update_delete),
 ]
