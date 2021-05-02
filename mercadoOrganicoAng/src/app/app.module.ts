a97e5df1d340e2e1585656364377b63113af190f
Alex Romero <alex4543@hotmail.com>
2021-05-02 13:59:57 -0500

    angular carrito compra

--- mercadoOrganicoAng/src/app/app.module.ts
+++ mercadoOrganicoAng/src/app/app.module.ts
@@ -7,17 +7,21 @@ import { AppComponent } from './app.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { AddClientComponent } from './components/add-client/add-client.component';
+import { CarritoItemCompraComponent } from './components/carrito-item-compra/carrito-item-compra.component';
 import { ClientService } from './client.service';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { CarritoConfirmarCompraComponent } from './components/carrito-confirmar-compra/carrito-confirmar-compra.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddProductComponent,
     CatalogoComponent,
-    AddClientComponent
+    CarritoItemCompraComponent,
+    AddClientComponent,
+    CarritoConfirmarCompraComponent
   ],
   imports: [
     BrowserModule,
